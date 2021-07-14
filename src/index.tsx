2377c45790483b575a04f0ea8f43a8df0b296e82
Conrad4 <1534409953@qq.com>
2021-07-14 22:53:27 +0800

    chore: 安装 jira devtool

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DevTools, loadServer } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+loadServer(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppProviders>
+        <DevTools />
+        <App />
+      </AppProviders>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
