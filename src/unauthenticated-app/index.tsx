16e8f544f3f481ddd9bbf44b39b500da25845b65
Conrad4 <1534409953@qq.com>
2021-07-23 23:29:13 +0800

    style: 安装emotion，优化部分unauth-index界面

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,18 +1,26 @@
+import styled from '@emotion/styled';
 import { Card } from "antd";
 import React, { useState } from "react";
 import { LoginScreen } from 'unauthenticated-app/login';
 import { RegisterScreen } from 'unauthenticated-app/register';
-
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
 
 
-  return <div style={{ display: "flex", justifyContent: "center" }}>
+  return(
+  <Container style={{ display: "flex", justifyContent: "center" }}>
     <Card>
-      { isRegister ? <RegisterScreen /> : <LoginScreen /> }
+      {isRegister ? <RegisterScreen /> : <LoginScreen />}
       {/* 怎么去实现切换功能，使用setState啊*/}
       <button onClick={() => { setIsRegister(!isRegister) }}> 切换到{isRegister ? '登录' : '注册'}</button>
     </Card>
-  </div>
+  </Container>
+  )
 };
 
+const Container = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`
