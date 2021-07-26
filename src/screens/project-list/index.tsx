e5c40d58b716dbc2cd6f468cfa016ec52f16d4d5
Conrad4 <1534409953@qq.com>
2021-07-26 23:26:57 +0800

    feat: 全部完成用utils/useAsync，useProject, useUser处理loading和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -5,6 +5,7 @@ import { useDebounce } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -14,6 +15,7 @@ export const ProjectListScreen = () => {
     personId: "",
   });
   const debouncedParam = useDebounce(param, 200);
+  // 又提取了一个useProjects 自定义hook，useProjects里面又嵌套着有useAsync处理loading和error
   const { isLoading, error, data: list } = useProjects(debouncedParam);
   const { data: users } = useUsers();
 
