91639b5c89e276a2e1df5fe100b77927bc31622c
Conrad4 <1534409953@qq.com>
2021-08-01 23:42:41 +0800

    docs: 修改理解的注释，不用闭包，使用useRef实现useDocumentTitle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -105,6 +105,7 @@ export const useArray = <T>(initialArray: T[]) => {
 };
 
 export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
+  // 使用useRef将数据持久化来满足需求，而不是使用闭包特性
   const oldTitle = useRef(document.title).current;
 
   // 页面加载时: 旧title
@@ -114,9 +115,9 @@ export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
   }, [title]);
 
   useEffect(() => {
-    // 组件卸载后，变回默认title，之后没有指定title的组件都将使用默认title
     return () => {
       if (!keepOnUnmount) {
+        // 在组件卸载后，变回默认title，之后没有指定title的组件都将使用旧title
         // 如果不指定下面[ ]的依赖[keepOnUnmount, oldTitle]，读到的就是旧title
         document.title = oldTitle;
       }
